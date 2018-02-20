3dd760e12b363fa067b2d13b38dcfe06c876931b
= <=>
2018-02-20 16:31:29 +0100

    added forward rolling model fitting

--- workshop_library/__init__.py
+++ workshop_library/__init__.py
@@ -1,6 +1,15 @@
 import seaborn as sns
+
 sns.set_style('darkgrid')
 sns.set_context('talk')
 import pylab
 import warnings
+
 warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+
+
+ms = pd.DataFrame({'A': np.random.randn(20), 'B': np.random.randn(20), 'C': np.random.randn(20)},
+                  index=pd.date_range(start='2100-01-01', periods=20))
--- workshop_library/training.py
+++ workshop_library/training.py
@@ -8,6 +8,11 @@ def get_X_y(df, target_column, hide_columns, date_column):
     if not df.index.name == date_column:
         df.set_index(date_column, inplace=True)
 
+    df.index = pd.to_datetime(df.index)
+
+    if not df.index.is_monotonic_increasing:
+        raise AttributeError('provided date column is not monotonically increasing')
+
     y = df[target_column].copy()
     hide_columns.append(target_column)
 
@@ -15,7 +20,7 @@ def get_X_y(df, target_column, hide_columns, date_column):
     return X, y
 
 
-def train_model_and_apply_test(x_train, x_test, y_train, y_test, results, model):
+def train_model_and_apply_test(x_train, x_test, y_train, y_test, results, model, round = 0):
     """apply training and test"""
     model.fit(x_train, y_train)
     pred = model.predict(x_test)
@@ -24,6 +29,7 @@ def train_model_and_apply_test(x_train, x_test, y_train, y_test, results, model)
     results['prediction'].extend(pred)
     results['truth'].extend(y_test)
     results['date'].extend(x_test.index)
+    results['round'].extend([round]*x_test.shape[0])
 
     return results
 
@@ -31,7 +37,7 @@ def train_model_and_apply_test(x_train, x_test, y_train, y_test, results, model)
 def training(
         df=None,
         backtest_settings=None,
-        target='',
+        target=None,
         hide_columns=None,
         model=None,
         date_column=None,
@@ -46,8 +52,17 @@ def training(
     :return: training_results
     """
 
+    if target is None:
+        raise SystemExit('A target column needs to be provided')
+    if model is None:
+        raise SystemExit('A valid model needs to be provided')
+    if hide_columns is None:
+        hide_columns = []
+
+
     # update the default backtest settings (bs)
-    bs = {'backtest_method': 'simple_split', 'split_ratio': 0.7, 'step_size': 10, 'training_window': 1000}
+    bs = {'backtest_method': 'simple_split', 'split_ratio': 0.7, 'step_size': 10, 'training_window': 1000,
+          'test_train_diff_days': 1}
     bs.update(backtest_settings)
 
     X, y = get_X_y(df=df, date_column=date_column, hide_columns=hide_columns, target_column=target)
@@ -60,9 +75,42 @@ def training(
         y_train, y_test = y[:cutoff], y[cutoff:]
 
         results = train_model_and_apply_test(x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test,
-                                             model=model, results=results)
+                                             model=model, results=results, round = 0)
 
-    res_df = pd.DataFrame({'prediction': results['prediction'], 'truth': results['truth']}, index=results['date'])
+    elif bs['backtest_method'] in ['walk_forward_rolling', 'walk_forward_extending']:
+        selection_dates = X.index.unique()
+        n_dates = len(selection_dates)
+
+        if n_dates < X.shape[0] - 5:
+            raise ValueError('provided data has not enough rows to perform a rolling model bulding')
+
+        test_start_idx = bs['training_window'] + bs['test_train_diff_days']
+        i = 0
+        while test_start_idx < df.shape[0] - bs['step_size']:
+            train_start_idx = 0 if bs['backtest_method'] == 'walk_forward_extending' else bs['step_size'] * i
+            train_end_idx = bs['training_window'] + i * bs['step_size']
+            test_start_idx = train_end_idx + bs['test_train_diff_days']
+            test_end_idx = test_start_idx + bs['step_size']
+            this_round_train_dates = selection_dates[train_start_idx:train_end_idx]
+            this_round_test_dates = selection_dates[test_start_idx:test_end_idx]
+            print(this_round_train_dates)
+            print(len(this_round_train_dates))
+            print(this_round_test_dates)
+            print(len(this_round_test_dates))
+
+            results = train_model_and_apply_test(
+                x_train=X.loc[this_round_train_dates],
+                x_test=X.loc[this_round_test_dates],
+                y_train=y.loc[this_round_train_dates],
+                y_test=y.loc[this_round_test_dates],
+                model=model,
+                results=results,
+                round = i
+            )
+            i += 1
+
+    res_df = pd.DataFrame({'prediction': results['prediction'], 'truth': results['truth'], 'round': results['round']},
+                          index=results['date'])
     return res_df
 
 
