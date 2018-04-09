dd389141aeaaf24928778424bfb0c4da9aa49c97
= <=>
2018-04-09 10:27:20 +0200

    added file for distribution

--- distribute_to_participants.py
+++ distribute_to_participants.py
@@ -5,9 +5,9 @@ participants = ['Test Teilnehmer 1']
 contents = ['Introduction to Python in Jupyter Notebooks.ipynb']
 
 for participant in participants:
-    dest_folder = '/home/user/Workshop/%s' % participant.replace(' ', '\ ')
+    dest_folder = '/home/user/Workshop/%s' % participant.replace(' ', '\\ ')
     os.makedirs(dest_folder, exist_ok=True)
     print('Copying Files for %s' % participant)
     for content in contents:
-        shutil.copyfile(src='/home/user/Workshop/AlexanderBeck/%s' % content.replace(' ', '\ '),
-                        dst='%s/%s' % (dest_folder, content.replace(' ', '\ ')))
+        shutil.copyfile(src='/home/user/Workshop/AlexanderBeck/%s' % content.replace(' ', '\\ '),
+                        dst='%s/%s' % (dest_folder, content.replace(' ', '\\ ')))
